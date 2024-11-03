6603ac49adc010f7498ea154abebf0886af090b8
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-11-03 17:37:21 +0700

    remove old library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,9 @@ dependencies {
 //    tensorflow
     implementation("org.tensorflow:tensorflow-lite:2.13.0")
     implementation("org.tensorflow:tensorflow-lite-api:2.13.0")
-    implementation("org.tensorflow:tensorflow-lite-support:0.4.0")
     implementation("org.tensorflow:tensorflow-lite-metadata:0.4.0")
-    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.0")
+//    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 
     implementation("com.google.android.gms:play-services-tflite-support:16.1.0")
     implementation("com.google.android.gms:play-services-tflite-gpu:16.2.0")
