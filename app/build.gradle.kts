a8913ae394b7450a3e3c260d5983b97fb4221f17
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-11-03 17:29:44 +0700

    add tf-lite bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,9 +59,4 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite-support:0.4.0")
     implementation("org.tensorflow:tensorflow-lite-metadata:0.4.0")
     implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
-
-    implementation("com.google.android.gms:play-services-tflite-support:16.1.0")
-    implementation("com.google.android.gms:play-services-tflite-gpu:16.2.0")
-    implementation("org.tensorflow:tensorflow-lite-task-vision-play-services:0.4.2")
-    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
 }
--- app/src/main/java/com/dicoding/picodiploma/mycamera/ImageClassifierHelper.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/ImageClassifierHelper.kt
@@ -7,19 +7,15 @@ import android.os.SystemClock
 import android.util.Log
 import android.view.Surface
 import androidx.camera.core.ImageProxy
-import com.google.android.gms.tflite.client.TfLiteInitializationOptions
-import com.google.android.gms.tflite.gpu.support.TfLiteGpu
 import org.tensorflow.lite.DataType
-import org.tensorflow.lite.gpu.CompatibilityList
 import org.tensorflow.lite.support.common.ops.CastOp
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.task.core.BaseOptions
 import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
-import org.tensorflow.lite.task.gms.vision.TfLiteVision
-import org.tensorflow.lite.task.gms.vision.classifier.Classifications
-import org.tensorflow.lite.task.gms.vision.classifier.ImageClassifier
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
 
 class ImageClassifierHelper(
     var threshold: Float = 0.1f,
@@ -31,17 +27,7 @@ class ImageClassifierHelper(
     private var imageClassifier: ImageClassifier? = null
 
     init {
-        TfLiteGpu.isGpuDelegateAvailable(context).onSuccessTask { gpuAvailable ->
-            val optionsBuilder = TfLiteInitializationOptions.builder()
-            if (gpuAvailable) {
-                optionsBuilder.setEnableGpuDelegateSupport(true)
-            }
-            TfLiteVision.initialize(context, optionsBuilder.build())
-        }.addOnSuccessListener {
-            setupImageClassifier()
-        }.addOnFailureListener {
-            classifierListener?.onError(context.getString(R.string.tflitevision_is_not_initialized_yet))
-        }
+        setupImageClassifier()
     }
 
     private fun setupImageClassifier() {
@@ -49,14 +35,7 @@ class ImageClassifierHelper(
             .setScoreThreshold(threshold)
             .setMaxResults(maxResults)
         val baseOptionsBuilder = BaseOptions.builder()
-        if (CompatibilityList().isDelegateSupportedOnThisDevice){
-            baseOptionsBuilder.useGpu()
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1){
-            baseOptionsBuilder.useNnapi()
-        } else {
-            // Menggunakan CPU
-            baseOptionsBuilder.setNumThreads(4)
-        }
+            .setNumThreads(4)
         optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
 
         try {
@@ -72,12 +51,6 @@ class ImageClassifierHelper(
     }
 
     fun classifyImage(image: ImageProxy) {
-        if (!TfLiteVision.isInitialized()) {
-            val errorMessage = context.getString(R.string.tflitevision_is_not_initialized_yet)
-            Log.e(TAG, errorMessage)
-            classifierListener?.onError(errorMessage)
-            return
-        }
         if (imageClassifier == null) {
             setupImageClassifier()
         }
